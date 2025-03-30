5410147d815b330b312a16391ed2b6311b8f3457
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-03-30 13:58:41 +0200

    added register page

--- scrim-master-app/src/components/Register.tsx
+++ scrim-master-app/src/components/Register.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Container from "@mui/material/Container";
-import {FormControlLabel, Link, Paper, TextField} from "@mui/material";
+import { Link, Paper, TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
