de9f435dfd0c1433c8d1c72e9d91e0e3bf0e8eb3
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-04-01 22:41:34 +0200

    updated TeamController to use DTOs

--- src/main/java/com/woytuloo/ScrimMaster/Controllers/TeamController.java
+++ src/main/java/com/woytuloo/ScrimMaster/Controllers/TeamController.java
@@ -1,5 +1,7 @@
 package com.woytuloo.ScrimMaster.Controllers;
 
+import com.woytuloo.ScrimMaster.DTO.DTOMappers;
+import com.woytuloo.ScrimMaster.DTO.TeamDTO;
 import com.woytuloo.ScrimMaster.Models.Team;
 import com.woytuloo.ScrimMaster.Services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/team")
 public class TeamController {
@@ -27,15 +30,25 @@ public class TeamController {
             @RequestParam(required = false) String name) {
         if (teamId != null) {
             Optional<Team> team = teamService.getTeamById(teamId);
-            return team.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+            return team.map(value -> new ResponseEntity<>(DTOMappers.mapToTeamDTO(value), HttpStatus.OK))
                     .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
         }
         if (name != null) {
             List<Team> teams = teamService.getTeamByName(name);
-            return new ResponseEntity<>(teams, HttpStatus.OK);
+            if (teams.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            List<TeamDTO> teamDTOs = teams.stream().map(DTOMappers::mapToTeamDTO).toList();
+            return new ResponseEntity<>(teamDTOs, HttpStatus.OK);
         }
+
+
         List<Team> teams = teamService.getAllTeams();
-        return new ResponseEntity<>(teams, HttpStatus.OK);
+        if(teams.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<TeamDTO> teamDTOs = teams.stream().map(DTOMappers::mapToTeamDTO).toList();
+        return new ResponseEntity<>(teamDTOs, HttpStatus.OK);
     }
 
     @PostMapping
