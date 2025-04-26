30e3c6d19275fc512cc52240fc59a8cc845275c3
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-04-26 19:35:23 +0200

    updated match repository

--- src/main/java/com/woytuloo/ScrimMaster/Repositories/MatchRepository.java
+++ src/main/java/com/woytuloo/ScrimMaster/Repositories/MatchRepository.java
@@ -3,13 +3,31 @@ package com.woytuloo.ScrimMaster.Repositories;
 import com.woytuloo.ScrimMaster.Models.Match;
 import com.woytuloo.ScrimMaster.Models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
     public Optional<Match> findById(long id);
+    public void deleteById(long id);
 
-    Match getMatchById(Long id);
+    @Query("""
+        SELECT DISTINCT m
+        FROM Match m
+        JOIN m.teams t
+        WHERE t.teamName = :teamName
+    """)
+    public List<Match> findByTeamName(String teamName);
+
+    @Query("""
+        SELECT DISTINCT m
+        FROM Match m
+        JOIN m.teams t
+        JOIN t.players p
+        WHERE p.username = :username
+    """)
+    public List<Match> findByUserName(String username);
 }
